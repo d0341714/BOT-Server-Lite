12a98c5980bddc21b44f78a91a87de902e412c71
chunyu1202 <chunyu1202@gmail.com>
2019-02-18 15:28:57 +0800

    Update UDPServer_Simple.c

--- modules/UDPServer_Simple/UDPServer_Simple.c
+++ modules/UDPServer_Simple/UDPServer_Simple.c
@@ -4,6 +4,10 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
+#define LISTEN_PORT 9999
+#define tracked_object_data 4
+#define UDP_RECEIVE_TIMEOUT_IN_MILLISECONDS 5000
+
 int main(int argc, char* argv[])
 {
     WSADATA wsaData;
@@ -12,7 +16,7 @@ int main(int argc, char* argv[])
     struct sockaddr_in serAddr;
     struct sockaddr_in remoteAddr;
     int nAddrLen;
-    char recvData[4096];
+    char recvData[WIFI_MESSAGE_LENGTH];
     int ret;
     struct timeval timeout;
     int optval = 1;
@@ -36,13 +40,13 @@ int main(int argc, char* argv[])
     setsockopt(serSocket, SOL_SOCKET, SO_REUSEADDR, (const void*)&optval,
                sizeof(int));
 
-    timeout.tv_sec = 5000;
+    timeout.tv_sec = UDP_RECEIVE_TIMEOUT_IN_MILLISECONDS;
     setsockopt(serSocket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
 
     memset(&serAddr, 0, sizeof(serAddr));
 
     serAddr.sin_family = AF_INET;
-    serAddr.sin_port = htons(9999);
+    serAddr.sin_port = htons(LISTEN_PORT);
     serAddr.sin_addr.S_un.S_addr = INADDR_ANY;
     if(bind(serSocket, (SOCKADDR*)&serAddr, sizeof(serAddr)) == SOCKET_ERROR){
         printf("bind error !");
@@ -54,13 +58,15 @@ int main(int argc, char* argv[])
 
     while (1){
         nAddrLen = sizeof(remoteAddr);
-        ret = recvfrom(serSocket, recvData, sizeof(recvData), 0, (SOCKADDR*) &remoteAddr, &nAddrLen);
+        ret = recvfrom(serSocket, recvData, sizeof(recvData), 0,
+                       (SOCKADDR*) &remoteAddr, &nAddrLen);
         if (ret < 0){
             printf("recvfrom last error code: [%d]\n", WSAGetLastError());
         }else{
             poll_type = recvData[0];
-            if(recvData[0] == 4){
-                SQL_update_object_tracking_data(db, &recvData[1], strlen(&recvData[1]));
+            if(recvData[0] == tracked_object_data){
+                SQL_update_object_tracking_data(db, &recvData[1],
+                                                strlen(&recvData[1]));
             }
         }
     }
