f3e622c4aaee8edf2bc3e7fb07610dc9a78f7a15
chunyu1202 <chunyu1202@gmail.com>
2019-03-28 14:28:19 +0800

    Revise the code according to Jane's review

--- src/SqlWrapper.h
+++ src/SqlWrapper.h
@@ -49,38 +49,13 @@
 //#define debugging
 
 /* 
-  SQL_callback
-
-     The callback function used by database operations
-
-  Parameter:
-
-     NotUsed - Not used
-     argc - number of parameters
-     argv - the Parameters
-     azColName - column names
-
-  Return Value:
-
-     int: If return 0, everything work successful.
-         If not 0, Something wrong.
-*/
-/*
-static int SQL_callback(void *NotUsed,
-                        int argc,
-                        char **argv,
-                        char **azColName);
-*/
-
-
-/* 
   SQL_execute
 
      The execution function used by database operations
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
      sql_statement - The SQL statement to be executed
 
   Return Value:
@@ -94,11 +69,14 @@ static ErrorCode SQL_execute(void* db, char* sql_statement);
 /* 
   SQL_begin_transaction
 
-     Begin transaction of SQL database operations.
+     Begins transaction makes the operations specified by the follows SQL 
+     database statement a transaction and holds a lock on the table until 
+     the transaction either is committed or rollbed back. Begin transaction 
+     makes the starting boundary of a transaction.
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
   Return Value:
 
@@ -109,30 +87,30 @@ static ErrorCode SQL_begin_transaction(void* db);
 
 
 /* 
-  SQL_end_transaction
+  SQL_commit_transaction
 
-     End transaction of SQL database operations.
+     Commits the transaction marked by a previous begin transaction. 
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
   Return Value:
 
      ErrorCode - indicate the result of execution, the expected return code
                  is WORK_SUCCESSFULLY
 */
-static ErrorCode SQL_end_transaction(void* db);
+static ErrorCode SQL_commit_transaction(void* db);
 
 
 /* 
   SQL_rollback_transaction
 
-     Rollback transaction of SQL database operations.
+     Rolls back the transaction marked by a previous begine transaction.
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
   Return Value:
 
@@ -145,13 +123,13 @@ static ErrorCode SQL_rollback_transaction(void* db);
 /* 
   SQL_open_database_connection
 
-     Open the database connection for operations
+     Opens a database connection to the database backend server
 
   Parameter:
 
      db_filepath - the full file path of database file
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
   Return Value:
 
@@ -164,11 +142,11 @@ ErrorCode SQL_open_database_connection(char* db_filepath, void** db);
 /* 
   SQL_close_database_connection
 
-     Close the database connection
+     Closes a database connection
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
   Return Value:
 
@@ -181,11 +159,12 @@ ErrorCode SQL_close_database_connection(void* db);
 /* 
   SQL_vacuum_database();
 
-     Vacuum the database to collect garbage
+     Identifies space occupied by deleted rows and catelogues to be garbage 
+     collected.
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
   Return Value:
 
@@ -198,11 +177,13 @@ ErrorCode SQL_vacuum_database(void* db);
 /* 
   SQL_retain_data();
 
-     Vacuum the database to collect garbage
+     Deletes the rows and catelogues which are older than the specified number 
+     of hours ago.
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
+
      retention_hours - specify the hours for data retention
 
   Return Value:
@@ -216,18 +197,18 @@ ErrorCode SQL_retain_data(void* db, int retention_hours);
 /* 
   SQL_update_gateway_registration_status
 
-     Update the input gateways as registered.
+     Updates the input gateways as registered.
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
-     buf: an input string with the format below to specify the registered
-          gateways
+     buf - an input string with the format below to specify the registered
+           gateways
 
-         length;gateway_ip_1;gateway_ip_2;gateway_ip_3;
+           length;gateway_ip_1;gateway_ip_2;gateway_ip_3;
 
-     buf_len: Length in number of bytes of buf input string
+     buf_len - Length in number of bytes of buf input string
 
   Return Value:
 
@@ -242,20 +223,21 @@ ErrorCode SQL_update_gateway_registration_status(void* db,
 /* 
   SQL_query_registered_gateways
 
-     Return all the gateways with the corresponding health_status
+     Returns all the gateways with the corresponding health_status
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
-     health_status: the health_status to be queried
-     output: an output buffer to receive the query resutls. The result string
-              is in format below.
+     health_status - the health_status to be queried
 
-         length;gateway_ip_1;health_status_1;gateway_ip_2;health_status_2; \
-         gateway_ip_3;health_status_3;
+     output - pointer to the output buffer to receive the query resutls. The 
+              result string is in the format below.
 
-     output_len: The maximum length in number of bytes of output buffer.
+              length;gateway_ip_1;health_status_1;gateway_ip_2; \
+              health_status_2;gateway_ip_3;health_status_3;
+
+     output_len - The maximum length in number of bytes of output buffer.
 
   Return Value:
 
@@ -271,19 +253,19 @@ ErrorCode SQL_query_registered_gateways(void* db,
 /* 
   SQL_update_lbeacon_registration_status
 
-     Update the input lbeacons as registered.
+     Updates the status of the input lbeacons as registered.
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
-     buf: an input string with the format below to specify the registered
-          gateways.
+     buf - pointer to an input string with the format below to specify the 
+           registered gateways.
 
-         length;gateway_ip;uuid_1;registered_timestamp_GMT;uuid_2;\
-         reigstered_timestamp_GMT;
+           length;gateway_ip;uuid_1;registered_timestamp_GMT;uuid_2;\
+           reigstered_timestamp_GMT;
 
-     buf_len: Length in number of bytes of buf input string
+     buf_len - Length in number of bytes of buf input string
 
   Return Value:
 
@@ -298,18 +280,18 @@ ErrorCode SQL_update_lbeacon_registration_status(void* db,
 /* 
   SQL_update_gateway_health_status
 
-     Update health status of gateways
+     Updates the health status of the input gateways
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
-     buf: an input string with the format below to specify the health status
-          of gateways
+     buf - pointer to an input string with the format below to specify the 
+           health status of gateways
 
-         length;gateway_ip_1;health_status;gateway_ip_2;health_status;
+           length;gateway_ip_1;health_status;gateway_ip_2;health_status;
 
-     buf_len: Length in number of bytes of buf input string
+     buf_len - Length in number of bytes of buf input string
 
   Return Value:
 
@@ -324,19 +306,19 @@ ErrorCode SQL_update_gateway_health_status(void* db,
 /* 
   SQL_update_lbeacon_health_status
 
-     Update health status of lbeacons
+     Updates the health status of the input lbeacons
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
-     buf: an input string with the format below to specify the health status
-          of gateways
+     buf - pointer to an input string with the format below to specify the 
+           health status of gateways
 
-         length;gateway_ip;lbeacon_uuid_1;health_status;lbeacon_uuid_2;\
-         health_status;
+           length;gateway_ip;lbeacon_uuid_1;health_status;lbeacon_uuid_2;\
+           health_status;
 
-     buf_len: Length in number of bytes of buf input string
+     buf_len - Length in number of bytes of buf input string
 
   Return Value:
 
@@ -351,22 +333,22 @@ ErrorCode SQL_update_lbeacon_health_status(void* db,
 /* 
   SQL_update_object_tracking_data
 
-     Update data of tracked objects
+     Updates data of tracked objects
 
   Parameter:
 
-     db - the pointer pointing to database
+     db - a pointer pointing to the connection to the database backend server
 
-     buf: an input string with the format below to specify the health status
-          of gateways
+     buf - pointer to an input string with the format below to specify the 
+           health status of gateways
 
-         lbeacon_uuid;gateway_ip;object_type;object_number; \
-         object_mac_address_1;initial_timestamp_GMT_1; \
-         final_timestamp_GMT_1;rssi_1;object_type;object_number; \
-         object_mac_address_2;initial_timestamp_GMT_2; \
-         final_timestamp_GMT_2;rssi_2;
+           lbeacon_uuid;gateway_ip;object_type;object_number; \
+           object_mac_address_1;initial_timestamp_GMT_1; \
+           final_timestamp_GMT_1;rssi_1;object_type;object_number; \
+           object_mac_address_2;initial_timestamp_GMT_2; \
+           final_timestamp_GMT_2;rssi_2;
 
-     buf_len: Length in number of bytes of buf input string
+     buf_len - Length in number of bytes of buf input string
 
   Return Value:
 
