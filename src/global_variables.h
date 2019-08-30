8e684d08a278b7638cea9b9008bec25433d36680
chunyu1202 <chunyu1202@gmail.com>
2019-08-30 17:30:59 +0800

    Jane's review

--- src/global_variables.h
+++ src/global_variables.h
@@ -12,16 +12,17 @@
 
   File Name:
 
-     global_variable.h
+     global_variables.h
 
   File Description:
 
-     This file include the global variables and definition that used in BeDIS 
-     and Server but not always commonly used in Gateway and LBeacon.
+     This file include the declarations and definitions of global variables 
+     and definition used in BeDIS Server but not commonly used in Gateway and 
+     LBeacon.
 
   Version:
 
-     2.0, 20190718
+     2.0, 20190830
 
   Abstract:
 
@@ -37,23 +38,29 @@
   Authors:
 
      Jia Ying Shi  , littlestone1225@yahoo.com.tw
+     Chun-Yu Lai, chunyu1202@gmail.com
  */
 
 #ifndef GLOBAL_VARIABLE_H
 #define GLOBAL_VARIABLE_H
 
+/* Following common constants are defined in BeDIS.h as well, we define these 
+   two common constants here because the structures declared below need these 
+   constants for declaration. */
 
 /* Length of the LBeacon's UUID in number of characters */
 #define LENGTH_OF_UUID 33
+
+/* Number of characters in a Bluetooth MAC address */
+#define LENGTH_OF_MAC_ADDRESS 18
+
+
 
 /* Length of geo_fence unique key in byte */
 #define LENGTH_OF_GEO_FENCE_KEY 32
 
 /* Length of geo_fence name in byte */
 #define LENGTH_OF_GEO_FENCE_NAME 32
-
-/* Number of characters in a Bluetooth MAC address */
-#define LENGTH_OF_MAC_ADDRESS 18
 
 /* Maximum length of message to communicate with SQL wrapper API in bytes */
 #define SQL_TEMP_BUFFER_LENGTH 4096
@@ -91,7 +98,8 @@ typedef struct {
    char fences[MAXIMUM_LBEACONS_IN_GEO_FENCE_FENCE][LENGTH_OF_UUID];
    int monitor_types[MAXIMUM_MONITOR_TYPE_IN_GEO_FENCE];
    
-   /* The list head records the list of the geo fence */
+   /* The list entry for inserting the geofence rule into the list of the geo 
+      fences */
    List_Entry geo_fence_list_entry;
 
 } GeoFenceListNode;
@@ -109,33 +117,33 @@ typedef struct {
     this Server */
     int allowed_number_nodes;
 
-    /* The time interval in seconds for the Server sending request for health
-       reports from LBeacon */
+    /* The time interval in seconds between consecutive Server requests for 
+       health reports from LBeacons */
     int period_between_RFHR;
 
-    /* The time interval in seconds for the Server sending request for tracked
-       object data from LBeacon */
+    /* The time interval in seconds between consecutive Server requests for 
+       tracked object data from LBeacons */
     int period_between_RFTOD;
     
-    /* The time interval in seconds for the Server checking object location 
-    information */
+    /* The time interval in seconds between consecutive checks by the Server
+       for object location information */
     int period_between_check_object_location;
 
-    /* The time interval in seconds for the Server checking object activity 
-    information */
+    /* The time interval in seconds between consecutive checks by the Server 
+       for object activity information */
     int period_between_check_object_activity;
 
     /* The number of worker threads used by the communication unit for sending
-      and receiving packets to and from LBeacons and the sever. */
+      and receiving packets to and from gateway and the Server. */
     int number_worker_threads;
 
-    /* A port that Gateways are listening on and for the Server to send to. */
+    /* A port that gateways are listening on and for the Server to send to. */
     int send_port;
 
-    /* A port that the Server is listening for Gateways to send to */
+    /* A port that the Server is listening for gateways to send to */
     int recv_port;
 
-    /* A port that the database is listening on for the Server to send to */
+    /* A port that the database is listening on and for the Server to send to */
     int database_port;
 
     /* The name of the database */
@@ -147,7 +155,7 @@ typedef struct {
     /* The password for accessing to the database */
     char database_password[MAXIMUM_DATABASE_INFO];
 
-    /* The number of days in which we want to keep the data in the database */
+    /* The number of days in which data are kept in the database */
     int database_keep_days;
 
     /* Priority levels at which buffer lists are processed by the worker threads
@@ -157,19 +165,20 @@ typedef struct {
     int normal_priority;
     int low_priority;
 
-    /*the time window in which we treat this object as shown and visiable by 
-    BOT system*/
+    /* The length of the time window in which each object is shown and 
+       made visiable by BOT system. */
     int location_time_interval_in_sec;
 
-    /*the time window in which we treat this object as in panic situation if 
-    object (user) presses panic button within the interval.*/
+    /* The length of the time window in which the object is in the panic 
+       condition often the user of the object presses the panic buton of the 
+       object. */
     int panic_time_interval_in_sec;
 
-    /*the time window in which we treat this object as shown, visiable, and 
-    geofence violation.*/
+    /* The length of the time window in which an object is shown to be in 
+       geofence violation. */
     int geo_fence_time_interval_in_sec;
     
-    /*the time window in which we want to monitor the movement activity.*/
+    /* The length of the time window in which we want to monitor the movement activity.*/
     int inactive_time_interval_in_min;
 
     /*the time slot in minutes in which we calculate the running average of 
